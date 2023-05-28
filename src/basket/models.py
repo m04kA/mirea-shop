264e63a8052f564439765b884318f0448ce5e658
R3al1ty1 <dolfinikkonovalov@gmail.com>
2023-05-28 17:48:16 +0300

    Added router and models for orders

--- src/basket/models.py
+++ src/basket/models.py
@@ -4,55 +4,32 @@ from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
 from sqlalchemy import TIMESTAMP, Column, Integer, String, Boolean, Table, ForeignKey, MetaData
 from sqlalchemy.orm import relationship
 
-from src.auth.models import role
+from src.auth.models import User as user_model
+from src.items.models import Item as item_model
 from src.database import Base, metadata
 
-Order = Table(
-    "item_ingredient",
+order_item = Table(
+    "order_item",
     Base.metadata,
     Column("id", Integer, primary_key=True),
     Column("item_id", Integer, ForeignKey("item.id")),
-    Column("user_id", Integer, ForeignKey("user.id")),
-    Column("order_id", Integer, nullable=False),
+    Column("order_id", Integer, ForeignKey("order.id")),
     Column("created_at", TIMESTAMP, default=datetime.utcnow),
     Column("updated_at", TIMESTAMP, default=datetime.utcnow, onupdate=datetime.utcnow)
 )
 
-class Item(Base):
-    __tablename__ = "item"
+class Order(Base):
+    __tablename__ = "order"
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)
-    price = Column(Integer, nullable=False)
-    description = Column(String)
-    item_type = Column(ForeignKey("type_item.id"))
-    photo_path = Column(String)
-    available = Column(Boolean, default=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    orderNr = Column(String, nullable=False)
     created_at = Column(TIMESTAMP, default=datetime.utcnow)
     updated_at = Column(TIMESTAMP, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    ingredients = relationship(
-        "Ingredient",
-        secondary=order,
-        back_populates="items",
-        lazy="selectin"
-    )
-
-class User(SQLAlchemyBaseUserTable[int], Base):
-    __tablename__ = "user"
-
-    id = Column(Integer, primary_key=True)
-    email = Column(String, nullable=False)
-    username = Column(String, nullable=False)
-    registered_at = Column(TIMESTAMP, default=datetime.utcnow)
-    updated_at = Column(TIMESTAMP, default=datetime.utcnow, onupdate=datetime.utcnow)
-    role_id = Column(Integer, ForeignKey(role.c.id))
-    hashed_password: str = Column(String(length=1024), nullable=False)
-    is_active: bool = Column(Boolean, default=True, nullable=False)
-    is_superuser: bool = Column(Boolean, default=False, nullable=False)
-    is_verified: bool = Column(Boolean, default=False, nullable=False)
-
     items = relationship(
-        "Item",
-        secondary=order,
-        back_populates="ingredients"
+       "Item",
+       secondary=order_item,
+       back_populates="items",
+       lazy="selectin"
     )
+
--- src/basket/router.py
+++ src/basket/router.py
@@ -5,28 +5,17 @@ from sqlalchemy import select, insert, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from pydantic import parse_obj_as
 
+from src.basket.schemas import OrderCreate
 from src.database import get_async_session
-from src.basket.models import Item, User, Order
-from src.basket.schemas import ItemCreate, OrderCreate, OrderBase, Ingredient
-from src.basket.schemas import User as user_schema
-from src.basket.schemas import Item as item_schema
+from src.basket.models import Order as order_model
+from src.auth.models import User as user_model
+from src.items.models import Item as item_model
 from src.basket.schemas import Order as order_schema
-from src.items.models import item_ingredient
-
 router = APIRouter(
     prefix="/orders",
-    tags=["Oorders"]
+    tags=["Orders"]
 )
 
-router_item = APIRouter(
-    prefix="/items",
-    tags=["Items"]
-)
-
-router_users = APIRouter(
-    prefix="/users",
-    tags=["Users"]
-)
 # ---- ORDER ----
 
 @router.post('/', response_model=order_schema)
@@ -34,16 +23,11 @@ async def create_order(new_operation: OrderCreate, session: AsyncSession = Depen
 
     order_dict = new_operation.dict()
     items_ids = order_dict.pop('item')
-    users_ids = order_dict.pop('user')
-    db_order = Order(**order_dict)
-    for user_id in users_ids:
-        user = await session.get(User, user_id)
-        for item_id in items_ids:
-            item = await session.get(Item, item_id)
-            if item:
-                db_order.ingredients.append(item)
-        if user:
-            db_order.ingredients.append(user)
+    db_order = order_model(**order_dict)
+    for item_id in items_ids:
+        item = await session.get(item_model, item_id)
+        if item:
+            db_order.ingredients.append(item)
     session.add(db_order)
     await session.commit()
     await session.refresh(db_order)
@@ -53,50 +37,29 @@ async def create_order(new_operation: OrderCreate, session: AsyncSession = Depen
 
 @router.get("/{order_id}/")
 async def get_one_order(order_id: int, session: AsyncSession = Depends(get_async_session)):
-    return await session.get(Order, order_id)
+    return await session.get(order_model, order_id)
 
 
 @router.get("/all", response_model=List[order_schema])
 async def get_all_type_orders(session: AsyncSession = Depends(get_async_session)):
-    query = select(Order)
+    query = select(order_model)
     result = await session.execute(query)
     orders = result.scalars().all()
-    order_details = []
-    for order in orders:
-        query_items = select(Item).join(Order).where(Order.c.order_id == Item.id)
-        query_users = select(User).join(Order).where(Order.c.order_id == User.id)
-        items = await session.execute(query_items)
-        users = await session.execute(query_users)
-        items = items.scalars().all()
-        users = users.scalars().all()
-        item_summaries = []
-        user_summaries = []
-        for elem in items:
-            description = elem.description if elem.description else None
-            photo_path = elem.photo_path if elem.photo_path else None
-            available = elem.available if elem.available else None
-            item_summaries.append(Item(id=elem.id, name=elem.name, price = elem.price, description=description, item_type = elem.item_type, photo_path=photo_path, available=available))
-        for user in users:
-            email = user.email if user.email else None
-            user_summaries.append(User(id=user.id, username=user.username, email = email))
-        order_details.append(order_schema(
-            items=item_summaries,
-            orders=user_summaries
-        ))
-    return order_details
+    return orders
+
 
 @router.delete("/{order_id}/")
 async def delete_one_ingredient(order_id: int, session: AsyncSession = Depends(get_async_session)):
-    order = await session.get(Order, order_id)
+    order = await session.get(order_model, order_id)
     await session.delete(order)
     await session.commit()
     return {"status": "success"}
 
 @router.put('/{order_id}/', response_model=order_schema)
 async def update_ingredient(order_id: int, item_id: int, new_operation: OrderCreate, session: AsyncSession = Depends(get_async_session)):
-    db_order = await session.get(Order, order_id)
+    db_order = await session.get(order_model, order_id)
 
-#    query = select(Order).where(Order.c.order_id == order_id)
+#    query = select(order_model).where(order_model.c.order_id == order_id)
 #    result_orders_ids = await session.execute(query)
 #    old_ingredients_ids = result_orders_ids.scalars().all()
 #
